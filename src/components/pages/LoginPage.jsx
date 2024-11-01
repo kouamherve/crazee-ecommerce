76cc836b1762a3b025c4b99dfea2ea9c1941d448
Herve Kouam <kouamherve01@gmail.com>
2024-11-01 17:24:38 -0400

    ✨ F01: LOGIN > Add title and form

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,5 +1,33 @@
-import React from "react";
+import { useState } from "react";
 
 export const LoginPage = () => {
-  return <div>LoginPage</div>;
+  const [username, setUsername] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`Hi, ${username}`);
+    setUsername("");
+  };
+
+  const handleChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  return (
+    <div>
+      <h1>Welcome home!</h1>
+      <br />
+      <h2>Log In</h2>
+      <form action="submit" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter your username..."
+          value={username}
+          onChange={handleChange}
+          required
+        />
+        <button>Access your space</button>
+      </form>
+    </div>
+  );
 };
